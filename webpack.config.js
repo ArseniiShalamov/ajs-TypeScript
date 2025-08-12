a1a4c0f0dbe5b27aa62113085fce38a20faff4df
Arsenii Shalamov <107767938+ArseniiShalamov@users.noreply.github.com>
2025-08-12 20:02:08 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,58 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const { appendFile } = require('fs');
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.ts',
-  target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.[contenthash].js',
-    chunkFormat: 'array-push',
-    clean: true,
-    publicPath: ''
+    filename: 'appendFile.bundle.js',
   },
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        use: { loader: 'ts-loader', options: { transpileOnly: true } }, // не валим сборку из-за TS-ошибок
-        exclude: [/node_modules/, /_tests_/, /\.test\.[tj]sx?$/]
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
       },
-      { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.html$/, use: 'html-loader' },
-      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] }
-    ]
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader, 'css-loader',
+        ],
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+    ],
   },
-  resolve: { extensions: ['.tsx', '.ts', '.js'] },
+
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+
   plugins: [
-    new HtmlWebpackPlugin({ template: 'public/index.html', filename: 'index.html' }),
-    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' })
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
   ],
-  devtool: false
 };
